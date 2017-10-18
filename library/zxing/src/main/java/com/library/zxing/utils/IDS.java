e87bc79bbfa1833d2282aafacbc1140abb487e4c
Canglangweiwei <weiweistar@163.com>
2017-10-18 12:38:35 +0800

    优化部分功能

---
+++ library/zxing/src/main/java/com/library/zxing/utils/IDS.java
@@ -0,0 +1,15 @@
+package com.library.zxing.utils;
+
+/**
+ * 由于在库项目中无法使用ids定义常量，建立此类
+ * Created by LiuPeng on 2015/8/14.
+ */
+@SuppressWarnings("ALL")
+public class IDS {
+    public static final int DECODE = 101;
+    public static final int DECODE_FAILED = 102;
+    public static final int DECODE_SUCCEEDED = 103;
+    public static final int QUIT = 104;
+    public static final int RESTART_PREVIEW = 105;
+    public static final int RETURN_SCAN_RESULT = 106;
+}
